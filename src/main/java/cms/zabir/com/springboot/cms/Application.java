33110858572ceddb655a83f7699c02c378f36a91
Zabir Haider Khan <xabirkhan@gmail.com>
2020-09-21 00:01:07 +0200

    Initial Commit

---
+++ src/main/java/cms/zabir/com/springboot/cms/Application.java
@@ -0,0 +1,13 @@
+package cms.zabir.com.springboot.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
