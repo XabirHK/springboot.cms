46a2e6dbd61d1393f93c7de69decd3dd0bedf52d
Zabir Haider Khan <xabirkhan@gmail.com>
2020-11-15 22:46:35 +0100

    Fixed #15 added post service

---
+++ src/main/java/com/zabir/springboot/cms/services/PostService.java
@@ -0,0 +1,53 @@
+package com.zabir.springboot.cms.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zabir.springboot.cms.models.Post;
+import com.zabir.springboot.cms.repositories.PostRepository;
+
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post savePost(Post post) {
+		 return postRepository.save(post);
+    }
+
+    public List<Post> savePosts(List<Post> Posts) {
+        return postRepository.saveAll(Posts);
+    }
+
+    public List<Post> getPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(int id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+//    public Post getPostByName(String name) {
+//        return postRepository.findByName(name);
+//    }
+
+    public String deletePost(int id) {
+    	postRepository.deleteById(id);
+        return "Post removed !! " + id;
+    }
+    
+    
+    public Post updatePost(Post Post) {
+        Post existingPost = postRepository.findById(Post.getId()).orElse(null);
+
+        existingPost.setTitle(Post.getTitle());
+        existingPost.setDescription(Post.getDescription());
+        existingPost.setCategory(Post.getCategory());
+        existingPost.setPicture(Post.getPicture());
+       
+        return postRepository.save(existingPost);
+    }
+}
