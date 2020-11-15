6908c49d0a1173957ec63a75e48f0a274c5436ae
Zabir Haider Khan <xabirkhan@gmail.com>
2020-11-15 17:06:04 +0100

    fixed #17 User service

---
+++ src/main/java/com/zabir/springboot/cms/services/UserService.java
@@ -0,0 +1,53 @@
+package com.zabir.springboot.cms.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zabir.springboot.cms.models.User;
+import com.zabir.springboot.cms.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User saveUser(User user) {
+		 return userRepository.save(user);
+    }
+
+    public List<User> saveUsers(List<User> Users) {
+        return userRepository.saveAll(Users);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+//    public User getUserByName(String name) {
+//        return userRepository.findByName(name);
+//    }
+
+    public String deleteUser(int id) {
+    	userRepository.deleteById(id);
+        return "User removed !! " + id;
+    }
+    
+    
+    public User updateUser(User User) {
+        User existingUser = userRepository.findById(User.getId()).orElse(null);
+        
+        //return User.getName();
+        
+        existingUser.setName(User.getName());
+        existingUser.setEmail(User.getEmail());
+        existingUser.setRole(User.getRole());
+       
+        return userRepository.save(existingUser);
+    }
+}
