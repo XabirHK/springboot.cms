5904db474ecdbf5d057d71e833c72acb0b0b2ae8
Zabir Haider Khan <xabirkhan@gmail.com>
2020-11-16 00:20:42 +0100

    Fixed #16 BAnner Picture service

---
+++ src/main/java/com/zabir/springboot/cms/models/BannerPhoto.java
@@ -0,0 +1,56 @@
+package com.zabir.springboot.cms.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.websocket.Decoder.Text;
+
+@Entity
+public class BannerPhoto {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String title;
+	
+	@Column(columnDefinition = "text")
+	private String description;
+	private String picture;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+	    this.id = id;
+	}
+	
+	public String getTitle() {
+	    return title;
+	}
+	
+	public void setTitle(String title) {
+	    this.title = title;
+	}
+	
+	public String getDescription() {
+	    return description;
+	}
+	
+	public void setDescription(String description) {
+	    this.description = description;
+	}
+	
+	
+	public String getPicture() {
+	    return picture;
+	}
+	
+	public void setPicture(String picture) {
+	    this.picture = picture;
+	}
+	
+	
+
+}
