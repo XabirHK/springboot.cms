cb3e11c2ae338e2c5f5dc5bd3d131687aa98e6ee
Zabir Haider Khan <XabirHK@users.noreply.github.com>
2021-05-01 12:39:59 +0200

    update data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,3 @@
 INSERT INTO role (name) VALUES ('ROLE_ADMIN');
 INSERT INTO role (name) VALUES ('ROLE_MODERATOR');
 INSERT INTO role (name) VALUES ('ROLE_USER');
-
-
-INSERT INTO user_roles (user_id, roles_id) VALUES (1, 1);
-INSERT INTO user_roles (user_id, roles_id) VALUES (1, 2);
-
-
-INSERT INTO user (id, email, password, username) VALUES (1, 'xabir.khan@gmail.com', '$2a$10$nIitt.UQqhGV.V53l26TmeYNvDsQZ3Lap0VGAPfm2TRjoaZm0nz3K', 'xabir');
-INSERT INTO user (id, email, password, username) VALUES (2, 'sami@mou.de', '$2a$10$Qh8mtMCEopaRDIlCFYoli.l1zhOfMhCl8FpXHUEgHK7f6Z6e1K/.O', 'sami');
